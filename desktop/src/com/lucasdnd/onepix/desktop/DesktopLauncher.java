8b6611ca723cf8c4f1a18fd96ea7d9a910d04210
Lucas <lucasdnd@gmail.com>
2015-09-05 00:31:23 -0300

    Added project

---
+++ desktop/src/com/lucasdnd/onepix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lucasdnd.onepix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lucasdnd.onepix.OnePix;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OnePix(), config);
+	}
+}
