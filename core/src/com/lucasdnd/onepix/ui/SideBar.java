3899b2e4253c83d88838a9205c2130fc76aeb5f2
Lucas <lucasdnd@gmail.com>
2015-09-06 14:24:34 -0300

    Added basic stats to the sidebar

--- core/src/com/lucasdnd/onepix/ui/SideBar.java
+++ core/src/com/lucasdnd/onepix/ui/SideBar.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.lucasdnd.onepix.FontUtils;
+import com.lucasdnd.onepix.OnePix;
+import com.lucasdnd.onepix.gameplay.Player;
 
 public class SideBar {
 	
@@ -30,8 +32,56 @@ public class SideBar {
 		sr.rect(x, y, width, height);
 		sr.end();
 		
+		// Status bars
+		Player p = ((OnePix)Gdx.app.getApplicationListener()).getPlayer();
+		drawBarFill(sr, x + margin, height - margin,  width - margin * 2, p.getHealth());
+		drawBarFill(sr, x + margin, height - margin * 5,  width - margin * 2, p.getStamina());
+		drawBarFill(sr, x + margin, height - margin * 9, width - margin * 2, p.getFood());
+		drawBarFill(sr, x + margin, height - margin * 13, width - margin * 2, p.getDrink());
+		
+		drawBarFrame(sr, x + margin, height - margin, width - margin * 2, margin);
+		drawBarFrame(sr, x + margin, height - margin * 5, width - margin * 2, margin);
+		drawBarFrame(sr, x + margin, height - margin * 9, width - margin * 2, margin);
+		drawBarFrame(sr, x + margin, height - margin * 13, width - margin * 2, margin);
+		
 		// UI Text
-		font.drawWhiteFont("Health", x + margin, height - margin, true);
+		font.drawWhiteFont("Health",  x + margin, height - margin * 3, true);
+		font.drawWhiteFont("Stamina", x + margin, height - margin * 7, true);
+		font.drawWhiteFont("Hunger",  x + margin, height - margin * 11, true);
+		font.drawWhiteFont("Thirst",  x + margin, height - margin * 15, true);
+	}
+	
+	private void drawBarFrame(ShapeRenderer sr, float x, float y, float width, float height) {
+		final float lineWeight = 4f;
+		final float lineHeight = margin * 1.8f;
+		final float lineWidth = width + lineWeight;
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.WHITE);
+		
+		// Left
+		sr.rect(x, y, lineWeight, lineWeight - lineHeight);
+		
+		// Right
+		sr.rect(Gdx.graphics.getWidth() - margin, y, lineWeight, lineWeight - lineHeight);
+		
+		// Top
+		sr.rect(x, y, lineWidth, lineWeight);
+		
+		// Bottom
+		sr.rect(x, y - lineHeight, lineWidth, lineWeight); 
+		
+		sr.end();
+	}
+	
+	private void drawBarFill(ShapeRenderer sr, float x, float y, float width, int value) {
+		final float lineWeight = 4f;
+		final float lineHeight = margin * 1.8f;
+		final float lineWidth = width + lineWeight;
+		float lineValue = lineWidth * (value / 1000) - lineWeight;
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.LIGHT_GRAY);
+		sr.rect(x + lineWeight, y - lineHeight, lineValue, lineHeight);
+		sr.end();
 	}
 	
 	public int getWidth() {
