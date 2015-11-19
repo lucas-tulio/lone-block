21c6a6fef5f120151288630db46c94e488eb890c
Lucas <lucasdnd@gmail.com>
2015-11-19 11:03:09 -0200

    Added a credits file

--- desktop/src/com/lucasdnd/onepixel/desktop/DesktopLauncher.java
+++ desktop/src/com/lucasdnd/onepixel/desktop/DesktopLauncher.java
@@ -12,14 +12,4 @@ public class DesktopLauncher {
 		config.resizable = false;
 		new LwjglApplication(new OnePixel(), config);
 	}
-	
-	/**
-	 * Placement sound - DasDeer (freesound.org) http://freesound.org/people/DasDeer/sounds/161544/
-	 * Wood cutting sound - f4ngy (freesound.org) http://freesound.org/people/f4ngy/sounds/240788/
-	 * Stone mining sound (edited) - ryanconway (freesound.org) http://freesound.org/people/ryanconway/sounds/240801/
-	 * Leaves sound (edited) - ramagochi (freesound.org) http://freesound.org/people/ramagochi/sounds/119642/
-	 * Eating sound (edited) - niwki (freesound.org) http://freesound.org/people/niwki/sounds/169761/
-	 * Drinking sound (edited) - mshahen (freesound.org) http://freesound.org/people/mshahen/sounds/185456/
-	 * 
-	 */
 }
